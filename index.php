177d34dfb96e342d2af7a77d8a65276076d2cf60
Ga <gabri.magni.97@gmail.com>
2022-05-06 17:19:46 +0200

    style draft

--- index.php
+++ index.php
@@ -23,10 +23,25 @@ $faqs = [
 ];
 
 foreach( $faqs as $key => $faq ) {
-    echo $faq['question'] . "<br>" . $faq['answer'] . "<br>";
+    echo "<h2>" . $faq['question'] . "</h2>" . "<p>" . $faq['answer'] . "<p>";
     // foreach( $faq as $key => $element ) {
     //     echo $key . "<br>";
     // };
 };
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./assets/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
