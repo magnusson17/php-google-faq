9df6ad8dd19e603e42e61073d000e6fc63e46230
Ga <gabri.magni.97@gmail.com>
2022-05-07 14:03:26 +0200

    bootsrap

--- index.php
+++ index.php
@@ -23,27 +23,36 @@ $faqs = [
 ];
 
 // "<div>" .
-foreach( $faqs as $key => $faq ) {
-    echo "<h2>" . $faq['question'] . "</h2>" . "<p>" . $faq['answer'] . "<p>";
-    // foreach( $faq as $key => $element ) {
-    //     echo $key . "<br>";
-    // };
-};
 // . "</div>";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="./assets/style.css">
-    <title>Document</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" href="./assets/style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="d-flex justify-content-center">
+            <div class="w-75">
     
-</body>
+            <?php foreach( $faqs as $key => $faq ) {
+                echo "<h2>" . $faq['question'] . "</h2>" . "<p>" . $faq['answer'] . "<p>";
+                // foreach( $faq as $key => $element ) {
+                //     echo $key . "<br>";
+                // };
+            };
+            ?>
+    
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
 </html>
